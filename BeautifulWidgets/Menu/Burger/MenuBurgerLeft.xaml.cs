e6a44d86011bab4c28376b5b04f7526536f2180d
kaplacat <valnakin@hotmail.fr>
2021-10-05 17:40:27 +0200

    Ajoutez des fichiers projet.

---
+++ BeautifulWidgets/Menu/Burger/MenuBurgerLeft.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BeautifulWidgets.Menu.Burger
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuBurgerLeft : ContentView
+    {
+        const float FlyoutCornerRadius = 10f;
+
+        bool _isFlyoutOpen = false;
+        double _scale;
+        uint _flyoutSpeed = 200;
+        double _pagePositionX;
+        double _flyoutTranslationX;
+
+        public MenuBurgerLeft()
+        {
+            InitializeComponent();
+            _scale = MainContent.Scale;
+            _pagePositionX = MainContent.TranslationX;
+
+            MainContent.SizeChanged += OnMainContentSizeChanged;
+            Flyout.Opacity = 0;
+        }
+
+        void OnMainContentSizeChanged(object sender, EventArgs e)
+        {
+            MainContent.SizeChanged -= OnMainContentSizeChanged;
+            _flyoutTranslationX = MainContent.Width * .75;
+
+            if (Flyout.Children.Count == 1 && Flyout.Children[0] is Layout menuPage)
+            {
+                var flyoutPadding = Flyout.Width - (Flyout.Width * .8);
+                (Flyout.Children[0] as Layout).Padding = new Thickness(0, 0, flyoutPadding, 0);
+            }
+        }
+
+        void OnToggleMenu(object sender, EventArgs e)
+        {
+            ToggleFlyout();
+        }
+
+        void FlyoutClose(object sender, SwipedEventArgs e)
+        {
+            if (_isFlyoutOpen)
+                ToggleFlyout();
+        }
+
+        void FlyoutOpen(object sender, SwipedEventArgs e)
+        {
+            if (!_isFlyoutOpen)
+                ToggleFlyout();
+        }
+
+        void ToggleFlyout()
+        {
+            if (_isFlyoutOpen)
+            {
+                MainContent.ScaleTo(_scale, _flyoutSpeed);
+                MainContent.TranslateTo(_pagePositionX, Flyout.TranslationY, _flyoutSpeed);
+                Flyout.FadeTo(0, _flyoutSpeed);
+                MainContent.CornerRadius = 0;
+            }
+            else
+            {
+                MainContent.ScaleTo(_scale * .9, _flyoutSpeed);
+                MainContent.TranslateTo(Flyout.TranslationX + _flyoutTranslationX, Flyout.TranslationY, _flyoutSpeed);
+                Flyout.FadeTo(1, _flyoutSpeed);
+                MainContent.CornerRadius = FlyoutCornerRadius;
+            }
+
+            _isFlyoutOpen = !_isFlyoutOpen;
+        }
+    }
+}
---
+++ BeautifulWidgets/Profils/Round/RoundProfilName.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BeautifulWidgets.Profils.Round
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RoundProfilName : ContentView
+    {
+        public RoundProfilName()
+        {
+            InitializeComponent();
+        }
+    }
+}
